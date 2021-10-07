8b4ac3e01fc892e0645817d990768c3d2fd342d1
Ferro Boyd <764227+ferroboyd@users.noreply.github.com>
2021-10-07 11:51:06 -0500

    Created new parentcode file for iframe resizer

---
+++ parentcodenew.js
@@ -0,0 +1,26 @@
+function include(file) {
+  
+  var script  = document.createElement('script');
+  script.src  = file;
+  script.type = 'text/javascript';
+  script.defer = true;
+  
+  document.getElementsByTagName('head').item(0).appendChild(script);
+  
+}
+  
+/* Include Many js files */
+include('iframeResizer.min.js');
+
+iFrameResize({
+  log                     : true,                  // Enable console logging
+  enablePublicMethods     : true,                  // Enable methods within iframe hosted page
+  resizedCallback         : function(messageData){ // Callback fn when resize is received
+    console.log('CB');
+    console.log('Frame ID: ' + messageData.iframe.id);
+    console.log('Width: ' + messageData.height);
+    console.log('Height' + messageData.width);
+    console.log('Event type: ' + messageData.type);
+    console.log('--------');
+  }
+});
